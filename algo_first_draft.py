bba890a264b8cde9778f9e5b9c17a65db978b8ed
Modblitz <morgananthonyk@gmail.com>
2026-05-24 23:58:13 +0100

    comments

--- algo_first_draft.py
+++ algo_first_draft.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 # Reads wave file into memory
 wav_file = input("input test file here: ")
 
-
 audio, sr = sf.read(wav_file)
 
 # Restricts audio channel to use mono audio 
@@ -14,7 +13,7 @@ if audio.ndim > 1:
     audio = audio[:,0]
 
 frame_size = 1024
-hop = 512
+hop = 1024
 
 # Puts audio samples into frames
 frames = []
@@ -23,16 +22,8 @@ for i in range(0, len(audio) - frame_size, hop):
 
 frames = np.array(frames)
 
-# Implementing hanning window https://www.youtube.com/watch?v=1Hd72RpMFlQ
-window = np.hanning(frame_size)
-
-windows = []
-for i in frames:
-    windows.append(i * window)
-
-windows = np.array(windows)
 # fourier transforms
-spectra = np.fft.rfft(windows, axis=1)
+spectra = np.fft.rfft(frames, axis=1)
 
 compressed = spectra.copy() 
 
@@ -45,19 +36,7 @@ for i in range(compressed.shape[0]):
 #Inverse fourier transforms
 normal = np.fft.irfft(compressed, n=frame_size, axis=1)
 
-output_len = hop * (len(normal) - 1) + frame_size
-output = np.zeros(output_len)
-norm = np.zeros(output_len)
+output = normal.reshape(-1)
 
-# Overlap add
-for index, frame in enumerate(normal):
-    start = index*hop
-    output[start:start + frame_size] += frame * window
-    norm[start:start + frame_size] += window**2
-    
-#normalisation, there needs to be protection if norm has zero entries
-nonzero = norm > 1e-8
-output[nonzero] /= norm[nonzero]
-    
 #Writes compressed audio to output file
-sf.write(f"Output/testing_compressed.wav", output, sr)
+sf.write("Output/output.wav", output, sr)
