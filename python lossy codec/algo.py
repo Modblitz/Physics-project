75f3894061fdb6b47bc6f9ab1f882e87f69e8a97
Modblitz <morgananthonyk@gmail.com>
2026-05-31 05:01:39 +0100

    Revamped algo.py to work bytewise, unfinished and uncommented so please don't kill me

--- python lossy codec/algo.py
+++ python lossy codec/algo.py
@@ -10,10 +10,6 @@ debug = False
 wav_file = input("input test file here: ")
 if wav_file == '':
     wav_file = "test_audio/Track.wav"
-elif wav_file == "d":
-    wav_file = "test_audio/Track.wav"
-    debug = True
-
 file = False
 while not file:
     try:
@@ -54,27 +50,29 @@ compressed = spectra.copy()
 
 powers = []
 test = compressed[1]
+
+# Conceptual toy codec, comment this block when PAC is added (realistically should be commented after quantisation actually)
 # Removes frequencies of power that are not in the 80th percentile of power
-for i in range(compressed.shape[0]):
-    power = np.abs(compressed[i])**2
-    powers.append(power)
-    threshold = np.percentile(power, 80)
-    compressed[i][power < threshold] = 0
-    compressed[i] = np.round(np.real(compressed[i]), decimals=3) + 1j*np.round(np.imag(compressed[i]), decimals=3)
-   #psychoacoustics time
+# for i in range(compressed.shape[0]):
+#     power = np.abs(compressed[i])**2
+#     powers.append(power)
+#     threshold = np.percentile(power, 80)
+#     compressed[i][power < threshold] = 0
+
+# (eventually) psychoacoustics time
 for j in range(0, (frame_size+1)//2):
     if j < 20*frame_size/sr or j > 2e5*frame_size/sr:
        compressed[:, j] = 0
-   
-#frequency = (sr * (np.arange(len(compressed[0]))))/frame_size
-if debug:
-    for j in range(0, compressed.shape[0], 25):
-       # plt.plot(frequency, np.array(powers)[j])
-        plt.show()
 
-# print(frequency, np.array(powers)[1])
+# Quantisation
+step = 0.02
+q_real = np.round(compressed.real / step).astype(np.int32)
+q_imag = np.round(compressed.imag / step).astype(np.int32)
+
+spectra_hat = q_real.astype(np.float32) * step + 1j * q_imag.astype(np.float32) * step
+
 #Inverse fourier transforms
-normal = np.fft.irfft(compressed, n=frame_size, axis=1)
+normal = np.fft.irfft(spectra_hat, n=frame_size, axis=1)
 
 output_len = hop * (len(normal) - 1) + frame_size
 output = np.zeros(output_len)
