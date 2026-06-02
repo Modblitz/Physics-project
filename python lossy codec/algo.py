09ed2c7bedece44bf227e147e62a4036dfee7193
Modblitz <morgananthonyk@gmail.com>
2026-06-02 05:29:31 +0100

    I changed algo.py to be an actual codec

--- python lossy codec/algo.py
+++ python lossy codec/algo.py
@@ -1,101 +1,102 @@
+# Note: in the lab we will record an analogue signal, so we need to find put how to convert raw audio to wav before this works
 # Importing modules
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+
 import audiothings as at
 
-# Note: in the lab we will record an analogue signal, so we need to find put how to convert raw audio to wav before this works
 
-# Reads wave file into memory, will change this to byte
+
 debug = False # Literally does nothing
     
-wav_file = input("input test file here: ")
-if wav_file == '':
-    wav_file = "test_audio/Track.wav"
+codec = input("C for Compress or D for decompress")
+if codec in ('c', 'C'):
 
-with open(wav_file, "r+b") as file:
-    wav_bytes = file.read()
+    wav_file = input("input test file here: ")
+    if wav_file == '':
+        wav_file = "test_audio/Track.wav"
 
-wave_data = at.wave(wav_bytes)
+    with open(wav_file, "r+b") as file:
+        wav_bytes = file.read()
 
-signal, sr = wave_data.read_as_signal()
-signal = np.array(signal)
+    wave_data = at.wave(wav_bytes)
 
-# Restricts audio channel to use mono audio 
-if wave_data.channels > 1:
-    signal = signal[:, 0]
+    signal, sr = wave_data.read_as_signal()
+    signal = np.array(signal)
 
-frame_size = 1024
-hop = 512
+    # Restricts audio channel to use mono audio 
+    if wave_data.channels > 1:
+        signal = signal[:, 0]
 
-# Puts audio samples into frames
-frames = []
-for i in range(0, len(signal) - frame_size, hop):
-    frames.append(signal[i:i+frame_size])
-frames = np.array(frames)/(2**15)
+    frame_size = 1024
+    hop = 512
 
-# Implementing hanning window https://www.youtube.com/watch?v=1Hd72RpMFlQ
-window = np.hanning(frame_size)
+    # Puts audio samples into frames
+    frames = []
+    for i in range(0, len(signal) - frame_size, hop):
+        frames.append(signal[i:i+frame_size])
+    frames = np.array(frames)/(2**15)
 
-windows = []
-for i in frames:
-    windows.append(i * window)
+    # Implementing hanning window https://www.youtube.com/watch?v=1Hd72RpMFlQ
+    window = np.hanning(frame_size)
 
-windows = np.array(windows)
-# fourier transforms
-spectra = np.fft.rfft(windows, axis=1)
+    windows = []
+    for i in frames:
+        windows.append(i * window)
 
-compressed = spectra.copy() 
+    windows = np.array(windows)
+    # fourier transforms
+    spectra = np.fft.rfft(windows, axis=1)
 
-powers = []
-test = compressed[1]
+    compressed = spectra.copy() 
+    at.quantizer(spectra)
 
-# Conceptual toy codec, comment this block when PAC is added (realistically should be commented after quantisation actually)
-# Removes frequencies of power that are not in the 80th percentile of power
-# for i in range(compressed.shape[0]):
-#     power = np.abs(compressed[i])**2
-#     powers.append(power)
-#     threshold = np.percentile(power, 80)
-#     compressed[i][power < threshold] = 0
-
-# (eventually) psychoacoustics time
-# for j in range(0, (frame_size+1)//2):
-#     if j < 20*frame_size/sr or j > 2e4*frame_size/sr:
-#        compressed[:, j] = 0
-
-# # Quantisation
-# step = 0.02
-# q_real = np.round(compressed.real / step).astype(np.int32)
-# q_imag = np.round(compressed.imag / step).astype(np.int32)
-
-# spectra_hat = q_real.astype(np.float32) * step + 1j * q_imag.astype(np.float32) * step
-
-#Inverse fourier transforms
-normal = np.fft.irfft(compressed, n=frame_size, axis=1)
-
-output_len = hop * (len(normal) - 1) + frame_size
-output = np.zeros(output_len)
-norm = np.zeros(output_len)
-
-# Overlap add
-for index, frame in enumerate(normal):
-    start = index*hop
-    output[start:start + frame_size] += frame * window
-    norm[start:start + frame_size] += window**2
+    # Applying psychoacoustics (WIP)
+    #at.thresholding()
+        
+else:
+    # Decoding step
+    file = input("Input file to be decoded")
     
-#normalisation, there needs to be protection if norm has zero entries
-nonzero = norm > 1e-8
-output[nonzero] /= norm[nonzero]
+    blob = np.load(file)
+    step = float(blob["step"])
+    q_real = blob["q_real"]
+    q_imag = blob["q_imag"]
+    hop = blob["hop"]
+    frame_size = blob["frame_size"]
+    header = blob["header"]
+    
+    
+    compressed = step * q_real.astype(np.float32) + 1j * step * q_imag.astype(np.float32)
+    
+    #Inverse fourier transforms
+    normal = np.fft.irfft(compressed, n=frame_size, axis=1)
+    window = np.hanning(frame_size)
+    
+    # Overlap add
+    output_len = hop * (len(normal) - 1) + frame_size
+    output = np.zeros(output_len)
+    norm = np.zeros(output_len)
 
-output *= 32767
-output = np.clip(output, -32768, 32767).astype(np.int16)
+    for index, frame in enumerate(normal):
+        start = index*hop
+        output[start:start + frame_size] += frame * window
+        norm[start:start + frame_size] += window**2
+        
+    #normalisation, there needs to be protection if norm has zero entries
+    nonzero = norm > 1e-8
+    output[nonzero] /= norm[nonzero]
 
-with open("test_compressed.wav", "w+b") as file_out:
-    byte_array = []
-    byte_array.append(wave_data.header)
-    for sample in output:
-        byte_array.append(sample.tobytes())
-        byte_array.append(sample.tobytes())
-    file_out.write(b''.join(byte_array))
+    output *= 32767
+    output = np.clip(output, -32768, 32767).astype(np.int16)
+
+    # Writing to wav file
+    with open("test_compressed.wav", "w+b") as file_out:
+        byte_array = []
+        byte_array.append(header)
+        for sample in output:
+            byte_array.append(sample.tobytes())
+            byte_array.append(sample.tobytes())
+        file_out.write(b''.join(byte_array))
 
 print("Done!")
--- python lossy codec/audiothings.py
+++ python lossy codec/audiothings.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+# Utils, theres probably a library fo
 def byte_search(input, bytes):
+    #searches for byte sequences
     for i, byte in enumerate(input):
         if input[i:i+len(bytes)] == bytes:
             return i
@@ -16,7 +18,7 @@ class wave:
         fmt = byte_search(wav, b'fmt')
         self.data = byte_search(wav, b'data')
 
-
+        # Info from header
         self.audio_format = int.from_bytes(wav[fmt+8:fmt + 10], "little")
         self.channels = int.from_bytes(wav[fmt + 10:fmt + 12], "little")
         self.sample_rate = int.from_bytes(wav[fmt + 12:fmt + 16], "little")
@@ -44,7 +46,6 @@ class wave:
         sample_bytes = self.bits_per_sample // 8
         frame_bytes = self.bytes_per_bloc
         start = self.data_offset
-
         if channels == 1:
             samples = []
             for i in range(0, self.data_size, sample_bytes):
@@ -73,6 +74,7 @@ class wave:
 
         Essentially recovers the same function as how soundfile read audio files, but with the info in bytes
         Creates an array of the samples in with amplitude as floats
+        currently broken (sorry)
 
         '''
         channels = self.channels
@@ -96,18 +98,81 @@ class wave:
         else:
             print("TOO MUCH WORK I CBA!!!!")
             return
+    def write(self, path, output, data):
+        with open(path, "w+b") as file_out:
+            byte_array = []
+            byte_array.append(data.header)
+            for sample in output:
+                byte_array.append(sample.tobytes())
+                byte_array.append(sample.tobytes())
+            file_out.write(b''.join(byte_array))
+
+class encoder(wave):
+    def __init__(self, wav):
+        super().__init__(wav)
+
+def quantizer(spectra, sr, frame_size, hop, header):
+    num_bins = np.shape(spectra)[0]
+
+    step = 0.02
+    q_real = np.round(spectra.real / step).astype(np.int16)
+    q_imag = np.round(spectra.imag / step).astype(np.int16)    
+    np.savez(
+    "compressed.npz",
+    sr=sr,
+    frame_size=frame_size,
+    hop=hop,
+    step=step,
+    q_real=q_real,
+    q_imag=q_imag,
+    header = header
+)
+
+# Psychoacoustics
+
+def absolute_threshold(f):
+    '''
+    Outputs the absolute threshold power in Db at frequency f
+    '''
+    return 3.64*(f/1000)**(-0.8) - 6.5*np.exp(-0.6(f/1000-3.3)**2)+(10**-3)*(f/1000)**4
+
+def spread(masker_band, power, barks, f):
+    barks[masker_band] = power
+    barks[masker_band - 1] = 0.5*power
+    barks[masker_band + 1] = 0.5*power
+    
+
+def new_threshold(f, masker_band, barks):
+    
+    return max(absolute_threshold(f), spread) 
+    
+def thresholding(spectra, frame_size, sr):
+    for j in range(0, (frame_size+1)//2):
+        if j < 20*frame_size/sr or j > 2e4*frame_size/sr:
+            spectra[:, j] = 0
         
-# class FileTypeError(Exception):
-#     """Exception raised for custom error scenarios.
-
-#     Attributes:
-#         message -- explanation of the error
-#     """
-# 2
-#     def __init__(self, message, error_code):
-#         self.message = message
-#         self.error_code = error_code
-#         super().__init__(self.message)
-
-class encoder:
-    pass
+    bark_boundaries = [0, 100, 200, 
+                300, 400, 510, 
+                630, 770, 920, 
+                1080, 1270, 1480, 
+                1720, 2000, 2320,
+                2700, 3150, 3700,
+                4400, 5300, 6400,
+                7700, 9500, 12000, 15500, 20000]
+    bark_bands = 24*[[]]
+    bark_bands = dict()
+    cur_bin = 0
+    i = 0
+    while i in range(0, (frame_size+1)//2):
+        bark_powers = 24*[]
+        if i*sr/frame_size < bark_boundaries[cur_bin]:
+            bark_bands[i] = cur_bin
+            i += 1
+        else:
+            cur_bin += 1
+    
+    for frame_idx in spectra.shape[0]:
+        bark_powers = 24*[]
+        power = np.abs(spectra[frame_idx])**2
+        for bin in range(spectra.shape[1]):
+            bark_powers[bark_bands[bin]] += power[bin]
